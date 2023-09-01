5011408c0abcf83fd685958e305d03016f3585a6
rscres <rseelaen@student.42sp.org.br>
2023-09-01 20:20:28 -0300

    improve binary conversion(bit shift) and add signal to char conversion

--- client.c
+++ client.c
@@ -6,18 +6,46 @@
 /*   By: rseelaen <rseelaen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/23 12:25:35 by rseelaen          #+#    #+#             */
-/*   Updated: 2023/08/23 18:25:56 by rseelaen         ###   ########.fr       */
+/*   Updated: 2023/09/01 20:19:38 by rseelaen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
+int	to_binary(int c, int pid)
+{
+	int	shift;
+
+	shift = -1;
+	while (++shift < 8)
+	{
+		if (c & (0x01 << shift))
+		{
+			kill(pid, SIGUSR2);
+		}
+		else
+		{
+			kill(pid, SIGUSR1);
+		}
+		usleep(100);
+	}
+	return (1);
+}
+
 int	main(int argc, char **argv)
 {
-	if (argc != 2)
+	int	len;
+
+	if (argc != 3)
 	{
 		ft_printf("Error: incorrect number of arguments.");
+		return (1);
 	}
-	kill(atoi(argv[1]), SIGUSR1);
+	len = 0;
+	while (argv[2][len])
+	{
+		to_binary(argv[2][len++], atoi(argv[1]));
+	}
+	ft_printf("%d\n", len);
 	return (0);
 }
--- server.c
+++ server.c
@@ -6,7 +6,7 @@
 /*   By: rseelaen <rseelaen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/22 21:16:03 by rseelaen          #+#    #+#             */
-/*   Updated: 2023/08/23 18:25:53 by rseelaen         ###   ########.fr       */
+/*   Updated: 2023/09/01 19:18:19 by rseelaen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,37 @@
 
 void	handler(int num)
 {
-	num = 0;
-	ft_printf("here\n");
+	static int	c;
+	static int	i;
+
+	if (num == SIGUSR2)
+	{
+		c |= (0x01 << i);
+	}
+	i++;
+	if (i == 8)
+	{
+		write(1, &c, 1);
+		c = 0;
+		i = 0;
+	}
 }
 
 int	main(void)
 {
 	int	pid;
+	int	wait;
 
+	signal(SIGUSR1, handler);
+	signal(SIGUSR2, handler);
 	pid = getpid();
 	ft_printf("Server PID: %d\n", pid);
 	usleep(0.5 * 10e5);
 	ft_printf("Waiting for message...\n");
-	pause();
-	signal(SIGUSR1, handler);
+	wait = 1;
+	while (wait)
+	{
+		pause();
+	}
 	return (0);
 }
